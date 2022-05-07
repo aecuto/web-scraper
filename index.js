25950d7c58f0e4b52123ca4ea7ef2ff8af120cc6
aecuto <kampee.expertly01@gmail.com>
2022-05-07 12:12:29 +0700

    add write json file

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const fs = require("fs");
 
 const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
-const scraper = async (form) => {
+const json = { data: [] };
+let empty = 0;
+
+const scraper = (form) => {
+  let scraperData;
   const url = "http://www.oservicethai.com/tonpao/check_popup.php?checkit=on";
   request.post(
     {
@@ -16,26 +21,47 @@ const scraper = async (form) => {
       }
       const $ = cheerio.load(body);
 
+      const status = $("table b font").text();
+
       $("table b").remove();
-
       const data = $("table").text().split("  ");
-
-      if (data[2] === "คัมภีร์ ต้นอ้อย") {
-        console.log(data);
+      if (data[2]) {
+        scraperData = {
+          no: data[1],
+          name: data[2],
+          date: data[3],
+          time: data[4],
+          title: data[5],
+          details: data[6],
+          status,
+        };
+        json.data.push(scraperData);
+      } else {
+        empty += 1;
       }
     }
   );
+
+  return scraperData;
 };
 
 const main = async () => {
-  for (let index = 1; index <= 358; index++) {
+  for (let index = 1; index <= 1000; index++) {
     console.log("scraper: #", index);
     const form = {
       ID_CHECK: index,
       YOM_CHECK: 2565,
     };
     scraper(form);
-    await timer(1000);
+    await timer(250);
+
+    if (empty > 2) {
+      fs.writeFile("data.json", JSON.stringify(json), function (err) {
+        if (err) throw err;
+        console.log("complete");
+      });
+      break;
+    }
   }
 };
 
